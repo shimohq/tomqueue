7a260f583a2c3d5318fd51a4ef9afdf3e0a5f517
luin <i@zihua.li>
2016-08-30 12:10:52 +0800

    fix: calculate length of chinese letter correctly

--- test/utils.js
+++ test/utils.js
@@ -2,13 +2,37 @@
 
 const utils = require('../lib/utils');
 const expect = require('chai').expect;
+const Parser = require('redis-parser');
 
 describe('utils', function () {
   describe('.toPacket()', function () {
     it('returns correct results', function () {
-      expect(utils.toPacket('name')).to.eql('$4\r\nname\r\n');
-      expect(utils.toPacket({ a: { b: undefined } })).to.eql('*2\r\n$1\r\na\r\n$0\r\n');
-      expect(utils.toPacket({ a: { b: 'c' } })).to.eql('*2\r\n$1\r\na\r\n*2\r\n$1\r\nb\r\n$1\r\nc\r\n');
+      testIO({ a: { b: undefined } }, ['a', [] ], '*2\r\n$1\r\na\r\n*0\r\n');
+      testIO({ a: { b: 'c' } }, ['a', ['b', 'c']], '*2\r\n$1\r\na\r\n*2\r\n$1\r\nb\r\n$1\r\nc\r\n');
+      testIO({ a: { b: '我' } }, ['a', ['b', '我']], '*2\r\n$1\r\na\r\n*2\r\n$1\r\nb\r\n$3\r\n我\r\n');
+      testIO('name', 'name', '$4\r\nname\r\n');
     });
   });
 });
+
+function testIO(input, output, middleState) {
+  const packet = utils.toPacket(input);
+  expect(packet).to.eql(middleState);
+  const parser = new Parser({
+    returnReply: (reply) => {
+      send(reply);
+    },
+    returnError: (error) => {
+      send(error);
+    },
+    returnFatalError: (fatalError) => {
+      send(fatalError);
+    }
+  });
+
+  function send(reply) {
+    expect(output).to.eql(reply);
+  }
+
+  parser.execute(new Buffer(packet));
+}

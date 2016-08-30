e06e22e52e01212a1c8c4eb7a1cdab635ea40ea0
luin <i@zihua.li>
2016-08-30 11:34:05 +0800

    restore empty array

--- test/utils.js
+++ test/utils.js
@@ -7,7 +7,7 @@ describe('utils', function () {
   describe('.toPacket()', function () {
     it('returns correct results', function () {
       expect(utils.toPacket('name')).to.eql('$4\r\nname\r\n');
-      expect(utils.toPacket({ a: { b: undefined } })).to.eql('*2\r\n$1\r\na\r\n$-1\r\n');
+      expect(utils.toPacket({ a: { b: undefined } })).to.eql('*2\r\n$1\r\na\r\n$0\r\n');
       expect(utils.toPacket({ a: { b: 'c' } })).to.eql('*2\r\n$1\r\na\r\n*2\r\n$1\r\nb\r\n$1\r\nc\r\n');
     });
   });

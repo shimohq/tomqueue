5dd583d7a92416392c4e525f2b49694eb74215fe
luoyjx <yjk99@qq.com>
2017-04-26 16:56:11 +0800

    feature: redis storage simple implementation

--- examples/dispatcher_redis_test.js
+++ examples/dispatcher_redis_test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const uuid = require('uuid');
 const Redis = require('ioredis');
 const Dispatcher = require('../').Dispatcher;
 const dispatcher = new Dispatcher({
@@ -11,13 +12,13 @@ const dispatcher = new Dispatcher({
 
 dispatcher.start();
 
-let rev = 1;
 setInterval(function () {
+  const payloadId = uuid.v4();
   const payload = {
-    channel: 'abc' + rev,
-    rev: rev++
+    channel: 'abc' + payloadId,
+    rev: payloadId
   };
   dispatcher.send(payload).then((res) => {
-    console.log(res);
+    console.log('send success: ', res);
   }).catch(console.error);
-}, 500);
+}, 100);
